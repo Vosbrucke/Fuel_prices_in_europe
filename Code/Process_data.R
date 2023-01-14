a6cd42c17eef04d018309be3858219da1f1759d6
Vosbrucke <szymonlisowski99@gmail.com>
2023-01-14 12:38:33 +0100

    Initial commit

---
+++ Code/Process_data.R
@@ -0,0 +1,139 @@
+library(tidyverse)
+library(lubridate)
+library(openxlsx)
+library(rvest)
+
+# Read bulletin data
+oil_bulletin <- read.xlsx("https://ec.europa.eu/energy/observatory/reports/Oil_Bulletin_Prices_History.xlsx", sheet = "Prices with taxes, per CTR")
+
+# Gas oil de chauffage Heating gas oil Heizöl (II)	 Fuel oil - Schweres Heizöl (III) Soufre <= 1% Sulphur <= 1% Schwefel <= 1%	
+
+# Name columns
+colnames(oil_bulletin) <- letters[1:8]
+
+# Clean bulletin prices column for Pb95 
+oil_bulletin_prices <- oil_bulletin$c[grep("[0-9]+$", oil_bulletin$c)] %>% gsub(",", "", .)
+
+# Clean bulletin prices column for Diesel
+diesel_bulletin_prices <- oil_bulletin$d[grep("[0-9]+$", oil_bulletin$d)] %>% gsub(",", "", .)
+
+
+# Data for lpg is bugged- for some countries there are 4 products differentiated while for others 5 or 6. This can be fixed
+# using either calculation of how many observations there are, applying sequenced and repeated number to make groups and pivoting 
+# the data to filter for certain words appearing in the column. The alternative, probably faster option, would be to duplicate what
+# is in the last column with data to the point that all columns are filled- 5th and 6th in case of some countries
+# # Clean bulletin prices column for LPG
+# lpg_bulletin_prices <- oil_bulletin %>% 
+#   mutate(
+#     # Add a column for a situation when there are no data for a given date
+#   i = ifelse(grepl("[0-9]",a) & !grepl("[0-9]", h), 0, NA),
+#   h = ifelse(is.na(h), i, h)
+#   ) %>% 
+#   select(h) %>% 
+#   filter(!is.na(h)) %>% 
+#   pull()
+
+# Clean bulletin date column
+date <- oil_bulletin$a[grep("[0-9]+$", oil_bulletin$a)]
+
+# Correct date from integer to proper format
+date <- as_date(as.numeric(date), origin = "1899-12-30")
+
+# Delete NA's
+date <- date[!is.na(date)]
+
+# Drop NA's from oil bulletin
+# oil_bulletin <- oil_bulletin$a %>% drop_na()
+
+# Take countries from the oil bulletin data
+countries_vector <- oil_bulletin$a[grep("[A-Z]", oil_bulletin$a)]
+
+# Delete observation that are not countries names
+countries_vector <- countries_vector[str_length(countries_vector) == 2]
+
+# Check number of dates- there should be equal number to later add countries code to proper date points
+count <- data.frame(date) %>%
+  table()
+
+# Make a data frame 
+count <- data.frame(
+  char = names(count), 
+  count = as.numeric(count), 
+  stringsAsFactors=FALSE) 
+
+# Filter for date that appears 27 times. Check what date it is
+starting_date <- count %>%
+  filter(count == 27) %>%
+  head(n = 1) %>% 
+  select(char) %>% 
+  pull()
+
+# Make a tibble with date and oil prices column. Leave observations below starting date
+fuel_price <- tibble(date, Euro_super_95 = oil_bulletin_prices, Diesel = diesel_bulletin_prices) %>% filter(date >= as.Date(starting_date))
+
+# Make a vector with countries names
+countries <- rep(countries_vector, length.out = nrow(fuel_price)) %>% sort()
+
+# Make a final tibble. Change a oil price to reflect prices per 1 liter
+fuel_price_EU <- tibble(code = countries, fuel_price) %>% 
+  mutate(
+    Euro_super_95 = round(as.numeric(Euro_super_95)) / 1000,
+    Diesel = round(as.numeric(Diesel)) / 1000,
+    )
+
+# Change a code for Greece to 'EL'. This will be important in plotting when other eurostat databases use 'EL' code for Greece
+fuel_price_EU$code[fuel_price_EU$code == "GR"] <- "EL"
+
+# Read csv for countries full names
+countries_all <- read.csv("Processed_data/slim-2_pl.csv", sep = ";", na.strings = "blank") %>% 
+  select(-"country.code") %>% 
+  rename(country_name = name, country_name_pl = name_pl, code = "alpha.2")
+
+# Change Greece country code to be compatible with eurostat package
+countries_all$code[countries_all$code == "GR"] <- "EL"
+
+# Right join to code
+countries_eu <- countries_all %>% 
+  right_join(data.frame(code = c("AT", "BE", "BG", "CH", "CY", "CZ", "DE", "DK", "EE", "EL", "ES", "FI", "FR", "HR", "HU", "IE", "IS", "IT", "LT", "LU", "LV", "MK", "MT", "NL", "NO", "PL", "PT", "RO", "RS", "SE", "SI", "SK", "TR")), by = "code")
+
+fuel_price_EU %<>% 
+  left_join(countries_eu, by = "code")
+
+# Write csv with fuel price
+write_csv(fuel_price_EU, "Processed_data/fuel_price_EU.csv")
+
+
+# Make additional tibble for other regions
+additional_countries_code <-  tibble(code = c("EA19", "EA", "EU"), country_name = c("Euro area (19 countries)", "Euro area", "European Union"), country_name_pl = c("Kraje strefy euro (19 krajów)", "Kraje strefy euro", "Unia Europejska"))
+
+# Joing two data frames by binding rows
+countries_eu <- countries_eu %>% 
+  bind_rows(additional_countries_code) %>% 
+  arrange(code)
+
+# Write csv
+write_csv(countries_eu, "Processed_data/countries_eu_inflation.csv") 
+
+# Web scrape special aggregates for inflation categories
+special_aggregates <- read_html("https://ec.europa.eu/eurostat/ramon/nomenclatures/index.cfm?TargetUrl=LST_NOM_DTL&StrNom=HICP_2000&StrLanguageCode=EN&IntPcKey=37598921&StrLayoutCode=HIERARCHIC") %>% 
+  html_nodes(".text") %>% html_text2()
+
+# Select only upper elements in the list- the actual aggregates
+special_aggregates_u <- special_aggregates[
+  grepl("[[:upper:]]+$", special_aggregates)]
+
+# Adjust it
+special_aggregates_u <- special_aggregates_u[-c(1:3)]
+
+# Select only lower elements in the list- description of the aggregates
+special_aggregates_l <- special_aggregates[
+  grepl("[[:lower:]]", special_aggregates)]
+
+# Adjust it and remove some left overs
+special_aggregates_l <- special_aggregates_l[6:42] %>% stringr::str_remove("\n")
+
+# Make a tibble
+special_aggregates <- tibble(special_aggregates_u, special_aggregates_l)
+
+# Write csv
+write_csv(special_aggregates, "Processed_data/special_aggregates.csv")
---
+++ Code/Shiny.R
@@ -0,0 +1,230 @@
+library(shiny)
+library(shinythemes)
+library(plotly)
+library(wesanderson)
+library(ggtext)
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(patchwork)
+library(glue)
+library(ggh4x)
+
+ui <- fluidPage(
+  tags$head(tags$style(".well {background-color: #FFFFFF; border-color: #FFFFFF}")),
+  theme = shinytheme("cosmo"),
+  navbarPage(title = "Average fuel prices"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        "selected_country", 
+        "Select a country or countries to plot", 
+        choices = c('Austria', 'Belgium', 'Bulgaria', 'Cyprus', 'Czechia', 'Germany', 'Denmark', 'Estonia', 'Spain', 'Finland', 'France', 'Greece', 'Croatia', 'Hungary', 'Ireland', 'Italy', 'Lithuania', 'Luxembourg', 'Latvia', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Sweden', 'Slovenia', 'Slovakia'),
+        selected = "Austria",
+        multiple = TRUE
+      ),
+      selectInput(
+        "selected_fuel", 
+        "Select a fuel to plot", 
+        choices = list("Gasoline" = "Euro_super_95", "Diesel" = "Diesel"),
+        selected = ""
+      ),
+      dateRangeInput(
+        "date", "Choose date range",
+        start = Sys.Date() - years(3),
+        end = Sys.Date(),
+        startview = "year",
+        weekstart = 1
+      ),
+      tags$style(HTML(".datepicker {z-index:99999 !important;}"))
+    ),
+    mainPanel(
+      plotOutput(
+        "fuel_prices",
+        height = "400px"
+        )
+      )
+  )
+)
+
+server <- function(input, output) {
+  
+  # Read fuel price data
+  fuel_price_EU <- read_csv("Processed_data/fuel_price_EU.csv") %>% 
+    mutate(date = as.Date(date))
+  
+  palette <- reactive({wes_palette("Darjeeling1", n = length(input$selected_country), type = "continuous")})
+
+  date_start <- reactive({input$date[1] %>% 
+    as.Date(origin = "1970-01-01")})
+  date_end <- reactive({input$date[2] %>% 
+    as.Date(origin = "1970-01-01")})
+  
+  # Render plot
+  output$fuel_prices <- renderPlot({
+    tryCatch(
+    {
+      # Fuel name on a plot
+      fuel <- reactive({ifelse(input$selected_fuel == "Diesel", "Diesel", "Gasoline")})
+      
+      # Rename the selected fuel to plot it and remove observations after selected date_end
+      fuel_price_EU_0 <- reactive({fuel_price_EU %>% 
+        rename(fuel = input$selected_fuel) %>% 
+        filter(date < date_end())
+      })
+        
+      # Filter data from the date_start
+      react_df <- reactive({fuel_price_EU_0() %>% 
+        filter(date > date_start())
+      })
+    
+    
+      # Make a tibble to use in correct order as colors on the plot 
+      countries_letters <- tibble(
+        country_name = input$selected_country, 
+        color = letters[1:length(input$selected_country)]
+        )
+    
+      # Join two data frames and keep only selected countries
+      countries <- tibble(react_df()) %>% 
+        right_join(countries_letters, by = "country_name") 
+      
+      # Pull gasoil prices to use as 
+      y_line <- countries %>% 
+        group_by(country_name) %>% 
+        filter(row_number() == 1) %>% 
+        pull(fuel)
+      
+      # Add y axis lines that start and end when there are observations in those dates
+      y_axis_lines <- tibble(
+        x = rep(floor_date(as.Date(min(react_df()$date)), "year"), 5),
+        y = seq(0.5, 2.5, 0.5),
+        xend = rep(round_date(as.Date(max(react_df()$date)) + days(155), "year"), 5),
+        yend = seq(0.5, 2.5, 0.5)
+        )
+      
+      ggplot(data = fuel_price_EU_0(), aes(x = date, y = fuel, group = country_name)) +
+      # Make lines for y axis that does not go beyond scale_x_continuous limits
+      geom_segment(
+        data = y_axis_lines, 
+        mapping = aes(
+          x = x, 
+          y = y, 
+          xend = xend, 
+          yend = yend, 
+          group = y), 
+        linewidth = 0.1, 
+        color = "lightgrey"
+        ) +
+      # Make a line that goes from the end point, most recent date, to a country name
+      geom_segment(
+        data = data.frame(
+          x = as.Date(max(countries$date)),
+          y = y_line, 
+          xend = as.Date(max(countries$date) + days(60)),
+          yend = y_line),
+        mapping = aes(
+          x = x, 
+          y = y, 
+          xend = xend, 
+          yend = yend, 
+          color = unique(countries$color)
+          ),
+        # curvature = 0, 
+        linewidth = 0.2, 
+        inherit.aes = FALSE
+        ) +
+      # Make a country name label
+      geom_text(
+        data = data.frame(
+          x = as.Date(max(countries$date) + days(63)),
+          y = y_line
+          ),
+        mapping = aes(
+          x = x, 
+          y = y, 
+          color = unique(countries$color), 
+          label = unique(countries$country_name)
+          ),
+        size = 3, 
+        inherit.aes = FALSE, 
+        hjust = 0, 
+        fontface = "bold"
+        ) +
+      # Add lines for other countries to be in the background
+      geom_line(
+        colour = "#F0F0F0",
+        linewidth = 0.3
+        ) +
+      # Add lines for selected in the shiny app countries 
+      geom_line(
+        data = countries, 
+        aes(color = color),
+        linewidth = 0.75
+        ) +
+      # Add color palette to selected countries
+      scale_color_manual(
+        values = c(palette()),
+        name = NULL
+        ) +
+      # Make a hline for y axis at the 0 level
+      geom_segment(
+        aes(
+          x = floor_date(as.Date(min(react_df()$date)), unit = "year"), 
+          y = 0, 
+          xend = round_date(as.Date(max(react_df()$date)) + days(155), "year"), 
+          yend = 0), 
+        linewidth = 0.5
+        ) +
+      # Add points for each of the selected counties on the right side
+      geom_point(
+        data = countries %>% group_by(color) %>% filter(row_number() == 1),
+        aes(color = color),
+        size = 1, 
+        shape = 21, 
+        fill = "white", 
+        stroke = 1
+        ) +
+      # Change date to show full year data no matter the start to the end of the year; if the date_end is before 15th January plot until a year before
+      scale_x_date(
+        limits = c(floor_date(as.Date(min(react_df()$date)), "year"), round_date(as.Date(max(react_df()$date)) + days(155), "year") + days(round(0.18*(as.numeric(round_date(as.Date(max(react_df()$date))) + days(155), "year") - as.numeric(floor_date(as.Date(min(react_df()$date)), "year")))))),
+        expand = c(0,0),
+        breaks = seq.Date(floor_date(as.Date(min(react_df()$date)), "year"), round_date(as.Date(max(react_df()$date)) + days(155), "year"), "1 year"),
+        date_labels = "%Y",
+        minor_breaks = seq.Date(floor_date(as.Date(min(react_df()$date)), "year"), round_date(as.Date(max(react_df()$date)) + days(155), "year"), by = "quarter"),
+        guide = "axis_minor"
+        ) +
+      # Scale y from 0 to 2.5 with EUR labels 
+      scale_y_continuous(
+        breaks = seq(0, 2.5, 0.5), 
+        labels = paste0(seq(0, 2.5, 0.5), "€"),
+        limits = c(0, 2.75),
+        expand = c(0,0)
+        ) +
+      labs(
+        x = NULL, 
+        y = NULL, 
+        title = paste("Price of", tolower(fuel()), "among EU Member States on", format(max(react_df() %>% pull(date)), "%d %B %Y")), 
+        caption = paste("Data source: Oil Bulletin. European Commission, ", format(Sys.Date(), "%Y"), "\n; Author: Szymon Lisowski")
+        ) + 
+      theme_classic() +
+      theme(
+        axis.line.x = element_blank(),
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(face = "bold"),
+        plot.subtitle = element_text(size = 8),
+        plot.caption = element_text(hjust = 0, size = 4),
+        panel.grid.major = element_blank(),
+        axis.text = element_text(color = "black"),
+        axis.ticks = element_line(color = "black"),
+        axis.ticks.length.x = unit(0.25, "cm"),
+        legend.position = "none"
+        )
+    }, 
+    # If there is no country selected display empty plot
+    error = function(e) {""})
+  })
+}
+
+shinyApp(ui = ui, server = server)
